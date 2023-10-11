c4b682bc81e9921792f122f830719077a49848b9
Mager2112 <92055047+Mager2112@users.noreply.github.com>
2023-10-11 20:56:06 +0300

    Update Python_hash.py

--- Python_hash/Python_hash/Python_hash.py
+++ Python_hash/Python_hash/Python_hash.py
@@ -4,89 +4,47 @@
 #операцию удаления 
 #и операцию поиска пары по ключу
 #Класс Node помогает создать подобие односвязного списка
-class Node: 
-    def __init__(self, key, value): 
-        self.key = key 
-        self.value = value 
-        self.next = None
-  
-#Класс HashTable - это и есть реализация Хэш-таблицы
-class HashTable: 
-    def __init__(self, capacity): 
-        self.capacity = capacity 
-        self.size = 0
-        self.table = [None] * capacity 
-  
-    def _hash(self, key): 
-        return hash(key) % self.capacity 
-  
-    def insert(self, key, value): 
-        index = self._hash(key) 
-  
-        if self.table[index] is None: 
-            self.table[index] = Node(key, value) 
-            self.size += 1
-        else: 
-            current = self.table[index] 
-            while current: 
-                if current.key == key: 
-                    current.value = value 
-                    return
-                current = current.next
-            new_node = Node(key, value) 
-            new_node.next = self.table[index] 
-            self.table[index] = new_node 
-            self.size += 1
-  
-    def search(self, key): 
-        index = self._hash(key) 
-  
-        current = self.table[index] 
-        while current: 
-            if current.key == key: 
-                return current.value 
-            current = current.next
-  
-        raise KeyError(key) 
-  
-    def remove(self, key): 
-        index = self._hash(key) 
-  
-        previous = None
-        current = self.table[index] 
-  
-        while current: 
-            if current.key == key: 
-                if previous: 
-                    previous.next = current.next
-                else: 
-                    self.table[index] = current.next
-                self.size -= 1
-                return
-            previous = current 
-            current = current.next
-  
-        raise KeyError(key) 
-  
-    def __len__(self): 
-        return self.size 
-  
-    def __contains__(self, key): 
-        try: 
-            self.search(key) 
-            return True
-        except KeyError: 
-            return False
-  
-  
-# main
-if __name__ == '__main__': 
-  
-    #Создание хэш-таблицы, максимум элементов - 5
-    ht = HashTable(5) 
-    ht.insert("Пикулин", 5)
-    ht.insert("Тищенко", 4)
-    ht.insert("Бог-император", 9)
-    print ("Бог-император" in ht) #true
-    print("Смит" in ht) #false
-    print(ht.search("Бог-император"))
+class HashTable:
+    def __init__(self, size):
+        self.size = size
+        self.table = [None] * size
+
+    def hash_function(self, key):
+        return hash(key) % self.size
+
+    def add_element(self, key, value):
+        index = self.hash_function(key)
+
+        while self.table[index] is not None:
+            # Линейная проба: двигаемся к следующему слоту
+            index = (index + 1) % self.size
+
+        self.table[index] = (key, value)
+
+        # Проверка на переполнение (если таблица слишком заполнена)
+        if len([x for x in self.table if x is not None]) >= self.size * 0.7:
+            print("error 1")
+            self.resize()
+
+    def resize(self):
+        print("error 2")
+        # Увеличьте размер таблицы и перехешируйте элементы
+        self.size *= 2
+        new_table = [None] * self.size
+
+        for item in self.table:
+            if item is not None:
+                key, value = item
+                index = self.hash_function(key)
+
+                while new_table[index] is not None:
+                    index = (index + 1) % self.size
+
+                new_table[index] = (key, value)
+
+        self.table = new_table
+
+# Пример использования
+hash_table = HashTable(10)
+hash_table.add_element("key1", "value1")
+hash_table.add_element("key2", "value1")
